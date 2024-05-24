89bb92bba10cb377dde4d7592255a9f72e2edfad
Ishrakul Tahmid <ishrakultahmid@gmail.com>
2024-05-25 02:05:28 +0600

    Add files via upload

---
+++ Pattern/15.Upper_Left_Triangle.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+void pattern12(int n)
+{
+    for(int i=0; i<n; i++)
+    {
+        for(char ch='A'; ch<'A'+(n-i); ch++)
+        {
+            cout<<ch<<" ";
+        }
+        cout<<endl;
+    }
+}
+int main() 
+{
+    int n;
+    cin>>n;
+    pattern12(n);
+    return 0;
+}
