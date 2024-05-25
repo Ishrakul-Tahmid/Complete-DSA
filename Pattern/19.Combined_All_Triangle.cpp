30fd53002c5e0c378d4b590581255623cfc81df7
Ishrakul Tahmid <ishrakultahmid@gmail.com>
2024-05-25 17:40:24 +0600

    Update 19.Combined_All_Triangle.cpp

--- Pattern/19.Combined_All_Triangle.cpp
+++ Pattern/19.Combined_All_Triangle.cpp
@@ -1,3 +1,17 @@
+/*
+**********
+****  ****
+***    ***
+**      **
+*        *
+*        *
+**      **
+***    ***
+****  ****
+**********
+
+*/
+
 #include<bits/stdc++.h>
 using namespace std;
 void pattern19(int n)
